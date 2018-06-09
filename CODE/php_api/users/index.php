c6266e6b8f6adf54764feb441ebb42aaf6ecd162
bnwlkr <bnwlkr@icloud.com>
2018-06-09 10:38:34 -0700

    directory name change

--- CODE/php_api/users/index.php
+++ CODE/php_api/users/index.php
@@ -1,11 +1,10 @@
 <?php
-    
     $conn = oci_connect('ora_m3c1b', 'a34334169', 'ug');
     if (!$conn) {
         $e = oci_error();
         trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
     } else {
         echo "connected";
-    }    
+    }
     
 ?>
