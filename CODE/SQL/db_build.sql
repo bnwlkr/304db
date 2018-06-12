afcd99445f160794d3a1a4e834baef0c38ec6174
bnwlkr <bnwlkr@icloud.com>
2018-06-11 22:41:35 -0700

    testing out SQLBrain.php

--- CODE/SQL/db_build.sql
+++ CODE/SQL/db_build.sql
@@ -1,6 +1,56 @@
+--SCHEMA
+
 CREATE TABLE Agent(
         id INT PRIMARY KEY,
         name CHAR(20),
         email CHAR(30)
 );
+
+
+CREATE TABLE Exchange(
+        name CHAR(20) PRIMARY KEY, website CHAR(20)
+);
+
+CREATE TABLE Commodity(
+        name CHAR(20) PRIMARY KEY
+);
+
+CREATE TABLE Coin(
+        commodity_name CHAR(20) PRIMARY KEY,
+        FOREIGN KEY (commodity_name) REFERENCES Commodity(name)
+);
+
+CREATE TABLE Stock(
+        commodity_name CHAR(20) PRIMARY KEY,
+        FOREIGN KEY (commodity_name) REFERENCES Commodity(name)
+);
+
+CREATE TABLE Traded_On(
+        commodity_name CHAR(20),
+        exchange_name CHAR(20),
+        PRIMARY KEY (commodity_name, exchange_name),
+        FOREIGN KEY (commodity_name) REFERENCES Commodity(name),
+        FOREIGN KEY (exchange_name) REFERENCES Exchange(name)
+);
+
+
+--TRIGGERS
+
+/* not working, but important for the ISA hierarchy. Will figure out.
+CREATE TRIGGER Commodity.Create
+    BEFORE INSERT OR UPDATE OF commodity_name ON Coin
+    INSERT into Commodity values (commodity_name);
+*/
+
+
+--DUMMY-DATA
+
+
+
+
+
+
+
+
+
 
--- code/php_api/SQLBrain.php
+++ code/php_api/SQLBrain.php
@@ -1,22 +1,41 @@
 <?php
     
-    class SQLBrain  {
+spl_autoload_register(function ($class_name) {
+                          include $class_name . '.php';});
+class SQLBrain  {
+
+    private  $connection;
         
-        private $connection;
+    function start () {
+        $connection = oci_connect("ora_m3c1b", "a34334169", "//dbhost.ugrad.cs.ubc.ca:1522/ug");
+    }
         
-        static function start () {
-            $connection = oci_connect("ora_m3c1b", "a34334169", "//dbhost.ugrad.cs.ubc.ca:1522/ug");
+    /* finds commodities that match search parameters.
+     * assumes $search is accompanied by only one other parameter and $exchange_name is accompanied
+     * by no other parameters.
+     *
+     * @param $search - the search query
+     * @param $price_bounds - an associative array containg ['min'] and ['max'] price to search for
+     * @param $exchange_name - specifies that the search is for commodities on a particular exchange
+     */
+    function get_commodities($search=null, $price_bounds=null, $exchange_name=null) {
+        $ret = array();
+        if ($exchange_name != null) {
+            $query = "select commodity_name from Traded_On where exchange_name='$exchange_name'";
+            $run = oci_parse($this->connection, $query);
+            oci_execute($run);
+            oci_fetch_all($run, $res);
+            var_dump($res);
+        } else if ($price_bounds != null) {
         }
-        
-        /* finds commodities that match search parameters. */
-        static function getCommodities($search_query, $price_bounds=null, $exchange_name=null) {
-            return array();
-        }
-    
-        
-        
         
     }
+    
+    
+    
+    
+    
+}
 
 
 
