98c66e0b91cf871cf1b08ba1c92a2f8a16a337fc
bnwlkr <bnwlkr@icloud.com>
2018-06-11 23:22:37 -0700

    using commits to send code to server is b ad idea. new idea in the works.

--- CODE/php_api/index.php
+++ CODE/php_api/index.php
@@ -1,6 +1,17 @@
+
 <?php
-    
-    echo "root";
-    
-    
+
+$connection = oci_connect("ora_m3c1b", "a34334169", "//dbhost.ugrad.cs.ubc.ca:1522/ug");
+
+function do_query ($query) {
+    $run = oci_parse($connection, $query);
+    oci_execute($run);
+    oci_fetch_all($run, $res);
+    var_dump($res);
+}
+
+do_query("select * from Traded_On");
+
+
+
 ?>
--- code/php_api/classes/SQLBrain.php
+++ code/php_api/classes/SQLBrain.php
@@ -1,7 +1,5 @@
 <?php
-    
-spl_autoload_register(function ($class_name) {
-                          include $class_name . '.php';});
+
 class SQLBrain  {
 
     private  $connection;
@@ -18,17 +16,25 @@ class SQLBrain  {
      * @param $price_bounds - an associative array containg ['min'] and ['max'] price to search for
      * @param $exchange_name - specifies that the search is for commodities on a particular exchange
      */
-    function get_commodities($search=null, $price_bounds=null, $exchange_name=null) {
-        $ret = array();
+    function get_commodities($search, $min_price, $max_price, $exchange_name) {
         if ($exchange_name != null) {
-            $query = "select commodity_name from Traded_On where exchange_name='$exchange_name'";
-            $run = oci_parse($this->connection, $query);
-            oci_execute($run);
-            oci_fetch_all($run, $res);
-            var_dump($res);
-        } else if ($price_bounds != null) {
+            return $this->do_query("select commodity_name from Traded_On where exchange_name='$exchange_name'");
+        } else if ($min_price != null) {
+            return $this->do_query("");
         }
-        
+    }
+    
+    /*
+     * executes the given query and returns an array of resulting tuples
+     *
+     * @param $query - the query for the DB
+     */
+    
+    function do_query ($query) {
+        $run = oci_parse($this->connection, $query);
+        oci_execute($run);
+        oci_fetch_all($run, $res);
+        var_dump($res);
     }
     
     
