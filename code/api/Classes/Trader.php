4e5a4d4f2919afd50f6135fecde743941eb20c66
bnwlkr <bnwlkr@icloud.com>
2018-06-16 20:48:20 -0700

    buying and selling is now working

--- code/api/Classes/Trader.php
+++ code/api/Classes/Trader.php
@@ -17,35 +17,45 @@ class Trader
     }
 
     function buy ($user_id, $commodity_name, $exchange_name, $quantity) {
-        $user_balance = $this->get_balance($exchange_name, $user_id);
-        $ask = $this->get_ask($exchange_name, $commodity_name, $quantity);
+        $user_balance = $this->get_balance($exchange_name, 'cad', $user_id);
+        $ask = $this->get_ask($exchange_name, $commodity_name);
         $total = (double)$quantity * $ask;
-        if ($user_balance>=$total) {
-            $new_balance = $user_balance - $total;
-            $take = $this->sqlBrain->do_query("update Account
-                                              set value = $new_balance
+        if ($user_balance<$total) { return false;}
+        $take = $this->sqlBrain->do_query("update Account
+                                              set value = value - $total 
                                               where user_id=$user_id and commodity_name='cad' and exchange_name='$exchange_name'");
-            $give = $this->sqlBrain->do_query("update Account
-                                              set value = $new_balance
+        $give = $this->sqlBrain->do_query("update Account
+                                              set value = value + $quantity
                                               where user_id=$user_id and commodity_name='$commodity_name' and exchange_name='$exchange_name'");
-            return $take && $give;
-        } else {
-            return false;
-        }
+        return $take && $give;
     }
 
-    function get_balance ($exchange_name, $user_id) {
-        $result = $this->sqlBrain->do_query("select Account.value from Account where commodity_name='cad' and user_id=1 and exchange_name='$exchange_name'");
-        return (int)$result[0]['value'];
+    function sell  ($user_id, $commodity_name, $exchange_name, $quantity) {
+        $commodity_balance = $this->get_balance($exchange_name, $commodity_name, $user_id);
+        if ($commodity_balance<$quantity) {return false;}
+        $bid = $this->get_bid($exchange_name, $commodity_name);
+        $total = (double)$quantity * $bid;
+        $take = $this->sqlBrain->do_query("update Account
+                                              set value = value - $quantity 
+                                              where user_id=$user_id and commodity_name='$commodity_name' and exchange_name='$exchange_name'");
+        $give = $this->sqlBrain->do_query("update Account
+                                              set value = value + $total 
+                                              where user_id=$user_id and commodity_name='cad' and exchange_name='$exchange_name'");
+        return $take && $give;
     }
 
-    function get_ask ($exchange_name, $commodity_name, $quantity) {
+    function get_balance ($exchange_name, $commodity_name, $user_id) {
+        $result = $this->sqlBrain->do_query("select Account.value from Account where commodity_name='$commodity_name' and user_id=$user_id and exchange_name='$exchange_name'");
+        return (double)$result[0]['value'];
+    }
+
+    function get_ask ($exchange_name, $commodity_name) {
         $result = $this->sqlBrain->do_query("select Metric.ask from Metric join Traded_On on Metric.commodity_name = Traded_On.commodity_name and Metric.exchange_name = Traded_On.exchange_name where 
                                                     Traded_On.exchange_name='$exchange_name' and Traded_On.commodity_name ='$commodity_name'");
         return (double)$result[0]['ask'];
     }
 
-    function get_bid ($exchange_name, $commodity_name, $quantity) {
+    function get_bid ($exchange_name, $commodity_name) {
         $result = $this->sqlBrain->do_query("select Metric.bid from Metric join Traded_On on Metric.commodity_name = Traded_On.commodity_name and Metric.exchange_name = Traded_On.exchange_name where 
                                                     Traded_On.exchange_name='$exchange_name' and Traded_On.commodity_name ='$commodity_name'");
         return (double)$result[0]['bid'];
--- code/api/do_trade/index.php
+++ code/api/do_trade/index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 spl_autoload_register(function ($class_name) {
     include '../Classes/'. $class_name . '.php';});
 
-
+$move = $_GET['move'];
 $user_id = $_GET['user_id'];
 $commodity_name = $_GET['commodity_name'];
 $exchange_name = $_GET['exchange_name'];
@@ -15,4 +15,9 @@ $quantity = $_GET['quantity'];
 
 $trader = new Trader();
 
-$trader->buy($user_id, $commodity_name, $exchange_name, $quantity);
+if ($move=='buy') {
+    echo json_encode($trader->buy($user_id, $commodity_name, $exchange_name, $quantity));
+} else if ($move=='sell') {
+    echo json_encode($trader->sell($user_id, $commodity_name, $exchange_name, $quantity));
+}
+
