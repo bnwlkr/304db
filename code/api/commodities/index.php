5f576a0c1bef5e652306fb3e98ef4f6a51fce4b8
bnwlkr <bnwlkr@icloud.com>
2018-06-16 21:34:13 -0700

    update to make commodity search more diverse

--- code/api/commodities/index.php
+++ code/api/commodities/index.php
@@ -16,13 +16,25 @@ $commodity_name = $_GET['commodity_name'];
 
 $sqlBrain = new SQLBrain();
 
-if ($exchange_name != null) {  // list all the commodities on a particular exchange
-    $query = "select Metric.* from Metric join (select Commodity.name, Traded_On.exchange_name from Commodity join 
+$query10 = "select Metric.* from Metric join (select Commodity.name, Traded_On.exchange_name from Commodity join 
                   Traded_On on Commodity.name = Traded_On.commodity_name where Traded_On.exchange_name='$exchange_name') as 
                   comms on Metric.commodity_name = comms.name and comms.exchange_name = Metric.exchange_name";
-    if ($commodity_name != null) { $query .= " where Metric.commodity_name='$commodity_name'"; }
-    $ret = $sqlBrain->do_query($query);
-} else if ($max_price != null && $min_price != null) {
+$query01 = "select Metric.* from Metric join (select Commodity.name, Traded_On.exchange_name from Commodity join 
+                  Traded_On on Commodity.name = Traded_On.commodity_name) as 
+                  comms on Metric.commodity_name = comms.name and comms.exchange_name = Metric.exchange_name where Metric.commodity_name = '$commodity_name'";
+$query11 = "select Metric.* from Metric join (select Commodity.name, Traded_On.exchange_name from Commodity join 
+                  Traded_On on Commodity.name = Traded_On.commodity_name where Traded_On.exchange_name='$exchange_name') as 
+                  comms on Metric.commodity_name = comms.name and comms.exchange_name = Metric.exchange_name 
+                  where Metric.commodity_name='$commodity_name'";
+
+
+if ($exchange_name != null && $commodity_name == null) {  // list all the commodities on a particular exchange
+    $ret = $sqlBrain->do_query($query10);
+} else if ($exchange_name == null && $commodity_name != null) {
+    $ret = $sqlBrain->do_query($query01);
+} else if ($exchange_name != null && $commodity_name != null) {
+    $ret = $sqlBrain->do_query($query11);
+} else if ($min_price != null && $max_price != null) {
         $ret = $sqlBrain->do_query("select Metric.* from Metric join Commodity on Metric.commodity_name = Commodity.name 
                                               where Metric.ask BETWEEN $min_price and $max_price");
 }
