f1d2ef25d10215d9daceee28846abd7f96148a85
bnwlkr <bnwlkr@icloud.com>
2018-06-17 18:20:27 -0700

    another fantastic commit message

--- code/api/Classes/Arbitrageur.php
+++ code/api/Classes/Arbitrageur.php
@@ -22,12 +22,16 @@ class Arbitrageur
                                                           from ECOMM_METRICS a join ECOMM_METRICS b on b.commodity_name = a.commodity_name and 
                                                             not a.exchange_name = b.exchange_name where a.bid*(1.0-get_fees(a.exchange_name))>b.ask*(get_fees(b.exchange_name)+1.0)";
         if ($commodity_name) {
-            $result = $this->sqlBrain->do_query($query . " and a.commodity_name='eth'");
+            $result = $this->sqlBrain->do_query($query . " and a.commodity_name='$commodity_name'");
         } else {
             $result = $this->sqlBrain->do_query($query);
         }
-
         return $result;
+    }
+    function best_currencies () {
+
+
+
     }
 
 
