1db9fef93ba616a33bb4e6ad3771557065f72b87
bnwlkr <bnwlkr@icloud.com>
2018-06-18 08:49:22 -0700

    added display for json

--- code/console/display.php
+++ code/console/display.php
@@ -1,18 +1,38 @@
 <?php
 
 $ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "http://btfd.group/commodities/");
-
+curl_setopt($ch, CURLOPT_URL, "http://btfd.group/commodities/?exchange_name=QuadrigaCX");
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-
-$content = curl_exec ($ch);
-
+$result = json_decode(curl_exec ($ch));
 curl_close ($ch);
 
-var_dump($content);
+//var_dump($result);
+echo "
+<style>
+table, th, td {
+   border: 1px solid black;
+}
+</style>
+";
 
+$result_variable_names = array_keys(get_object_vars($result[0]));
 
+echo '<table>';
+echo '<tr>';
+foreach ($result_variable_names as $column_name) {
+    echo '<th>' . $column_name . '</th>';
+}
+echo '</tr>';
+
+foreach ($result as $obj) {
+    echo '<tr>';
+    foreach ($obj as $key => $value) {
+        echo '<th>' . $value . '</th>';
+    }
+    echo '</tr>';
+}
+
+echo '</table>';
 
 
 
